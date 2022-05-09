51b833efa1882d139c1ea7db8d7b9a62afbed3e1
Hung OCA <vhungit.it@gmail.com>
2022-05-09 17:30:56 +0700

    Commit

--- Project/tiktok-ui/src/components/Layouts/DefaultLayout/index.js
+++ Project/tiktok-ui/src/components/Layouts/DefaultLayout/index.js
@@ -1,11 +1,15 @@
+import classNames from 'classnames/bind';
 import Header from '~/components/Layouts/components/Header';
+import styles from './Default.module.scss';
 import Sidebar from './Sidebar';
+
+const cx = classNames.bind(styles);
 
 export default function DefaultLayout({ children }) {
   return (
-    <div>
+    <div className={cx('wrapper')}>
       <Header />
-      <div className="container">
+      <div className={cx('container')}>
         <Sidebar />
         <div className="content">{children}</div>
       </div>
--- Project/tiktok-ui/src/components/Layouts/components/Header/index.js
+++ Project/tiktok-ui/src/components/Layouts/components/Header/index.js
@@ -1,3 +1,12 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className={cx('wrapper')}>
+      <div className={cx('inner')}></div>
+    </header>
+  );
 }
